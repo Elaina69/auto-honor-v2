23b74154f776ea293cdb75bf984f38b2d3069188
@ Elaina @ <vipnoxhd123@gmail.com>
2025-08-31 17:42:29 +0700

    Fix wrong querySelector

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,14 @@ import { settings } from "./src/settings.ts"
 import { settingsUtils } from "./src/settingsUtils.ts"
 import * as upl from "pengu-upl"
 
-if (!window.DataStore.has("Auto-Honor-V2")) {
-    window.DataStore.set("Auto-Honor-V2", true)
-    window.DataStore.set("Auto-Honor-V2-Mode", 0)
-}
+if (!window.DataStore.has("Auto-Honor-V2")) window.DataStore.set("Auto-Honor-V2", true)
+if (!window.DataStore.has("Auto-Honor-V2-Mode")) window.DataStore.set("Auto-Honor-V2-Mode", 0)
+if (!window.DataStore.has("Auto-Honor-V2_skipHonor")) window.DataStore.set("Auto-Honor-V2_skipHonor", false)
 
 let honored = false
 
 let main = async (type: string) => {
-    if (!honored && window.DataStore.get("Auto-Honor-V2")) {
+    if (!honored && window.DataStore.get("Auto-Honor-V2") && !window.DataStore.get("Auto-Honor-V2_skipHonor")) {
         honored = true
 
         log("Start running...")
@@ -50,6 +49,14 @@ let observeHonorScreen = () => {
         log("Honor screen V3 detected.")
 
         await main("V3")
+    })
+
+    upl.observer.subscribeToElementCreation(".vote-ceremony-submit-button", async (element: any) => {
+        autoHonor.skipHonor(element);
+    })
+
+    upl.observer.subscribeToElementCreation(".vote-ceremony-v3-submit-button", async (element: any) => {
+        autoHonor.skipHonor(element);
     })
 }
 
